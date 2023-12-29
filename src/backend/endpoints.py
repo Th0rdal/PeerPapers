f919a55924449a9a83d8a14ece53677e0f0cd17e
Sweisser7 <simon.weisser@stud.fh-campuswien.ac.at>
2023-12-29 16:23:00 +0100

    login endpoint prototype

--- src/backend/endpoints.py
+++ src/backend/endpoints.py
@@ -27,13 +27,30 @@ def register ():
         return jsonify({'message': 'Username is already taken'}), 400
     except NoRowFoundException as e: #When NoRowFoundException is triggered, username is not taken.
         databaseAccess.newEntry(Table.AUTHENTICATION, {'username': username, 'password': password})
-    #databaseAccess.printTable(Table.AUTHENTICATION)
+    databaseAccess.printTable(Table.AUTHENTICATION)
     logging.info('Registration successful')
     return jsonify({'message': 'Registration successful'}), 200
 
 @app.route('/login', methods=['POST'])
 def login ():
-    pass
+    '''
+    Endpoint for user login with username and password. Username and password must be sent in method body.
+    '''
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    if not username or not password:
+        logging.info('Login not successful, username and password are required')
+        return jsonify({'error': 'Both username and password are required'}), 400
+    databaseAccess = DatabaseAccessObject()
+    databaseAccess.printTable(Table.AUTHENTICATION)
+    try:
+        databaseAccess.findOne(Table.AUTHENTICATION, {'username': username, 'password': password})
+        return jsonify({'message': 'Login successful'}), 200
+    except NoRowFoundException as e: #When NoRowFoundException is triggered, username is not taken.
+        logging.info('Login not successful, username or password incorrect')
+        return jsonify({'message': 'Username or Password incorrect'}), 400    
+    
     
 @app.route('/upload', methods=['POST'])
 def upload ():
