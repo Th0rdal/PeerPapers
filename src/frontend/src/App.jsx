f55fdde60e414ddef56fa9e0e71899e75f4bc1e1
tristan-cyber <tristanwestreicher@gmail.com>
2023-12-29 17:44:48 +0100

    added new navbar

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,27 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { PrimeReactProvider, PrimeReactContext } from "primereact/api";
-import "primereact/resources/themes/saga-blue/theme.css"; //theme
-import "primereact/resources/primereact.min.css"; //core css
-// import "primeicons/primeicons.css"; //icons
 
 // import of pages -> always also define the name of the page
 import Test from "./pages/test";
 import HomePage from "./pages/homePage";
 import Navbar from "./modules/components/navbar";
-import { MegaMenu } from "primereact/megamenu";
 
 function App() {
   return (
-    <PrimeReactProvider>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/test" element={<Test />} />
-        </Routes>
-      </Router>
-    </PrimeReactProvider>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/test" element={<Test />} />
+      </Routes>
+    </Router>
   );
 }
 
